6399a85fce755b1b3b0c72a57083beb2f7ecb4a1
Jocke <jocke@niden.se>
2015-12-23 11:33:17 +0100

    Fix dynamic source

--- scripts/app.js
+++ scripts/app.js
@@ -1,10 +1,30 @@
 
-/*var content = {
-   logo: "images/moshpitandcode_white.svg",
-   profilepic: 'images/me.jpg'
-};*/
+function Xhr(){ /* returns cross-browser XMLHttpRequest, or null if unable */
+    try {
+        return new XMLHttpRequest();
+    }catch(e){}
+    try {
+        return new ActiveXObject("Msxml3.XMLHTTP");
+    }catch(e){}
+    try {
+        return new ActiveXObject("Msxml2.XMLHTTP.6.0");
+    }catch(e){}
+    try {
+        return new ActiveXObject("Msxml2.XMLHTTP.3.0");
+    }catch(e){}
+    try {
+        return new ActiveXObject("Msxml2.XMLHTTP");
+    }catch(e){}
+    try {
+        return new ActiveXObject("Microsoft.XMLHTTP");
+    }catch(e){}
+    return null;
+}
 
-var url = 'http://moshpitandcode.com/content/content.json';
+var origin = window.location.origin;
+var path = window.location.pathname;
+
+var url = origin+path+'content/content.json';
 
 new Vue({
 	el: '#app',
@@ -27,7 +47,7 @@ new Vue({
 	},
 	methods: {
 		fetchData: function () {
-			var xhr = new XMLHttpRequest();
+			var xhr = Xhr();
 			var self = this;
 			xhr.open('GET', url);
 			xhr.onload = function () {
